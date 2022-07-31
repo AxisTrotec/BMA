09fe51428a74cf9afdbca9263a360aa8e9ef497c
Axis (Kelvin) <AxisTrotec@gmail.com>
2022-07-30 21:41:14 -0700

    Test: Test for CI Workflow

--- src/app.js
+++ src/app.js
@@ -28,11 +28,6 @@ app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'pug');
 
 //Configuring app
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
 
 //Initializing app connection with Routers
 app.use('/', indexRouter);
