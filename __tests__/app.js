b6b2e5542e90b36c8b1aa178f776d28880adfc14
Axis (Kelvin) <AxisTrotec@gmail.com>
2022-07-30 22:19:00 -0700

    Added: Test case for jest

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,36 +1,48 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
-describe('Create', () => {
-    let connect;
-    let db;
+describe("Create", () => {
+  let connect;
+  let db;
 
-    var mongoDB = 'mongodb+srv://admin:admin21321@cluster0.op2uu.mongodb.net/?retryWrites=true&w=majority'
+  var mongoDB =
+    "mongodb+srv://admin:admin21321@cluster0.op2uu.mongodb.net/?retryWrites=true&w=majority";
 
-    beforeAll(async () => {
-        connect = mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
-    })
-    
-    db = mongoose.connection;
-    db.on('error', console.error.bind(console, '[Error] MongoDB connection error'))
+  beforeAll(async () => {
+    connect = mongoose.connect(mongoDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  });
 
-    afterAll(async () => {
-        await connect.close()
-    })
-})
+  db = mongoose.connection;
+  db.on(
+    "error",
+    console.error.bind(console, "[Error] MongoDB connection error")
+  );
 
-it("Should create a fake account", () => {
-    const users = db.collection('users')
+  afterAll(async () => {
+    await connect.close();
+  });
 
-    const testUser = {username: "testUser", email: "testUser@test.com", phone: "7783201682", password: "test21321"}
-    users.insertOne(testUser)
+  it("Should create a fake account", () => {
+    const users = db.collection("users");
 
-    const created =  users.findOne({email: "testUser@test.com"})
-    expect(created.password).toEqual(testUser.password)
-})
+    const testUser = {
+      username: "testUser",
+      email: "testUser@test.com",
+      phone: "7783201682",
+      password: "test21321",
+    };
+    users.insertOne(testUser);
 
-it("Should be able to login", () => {
-    const users = db.collection('users')
+    const created = users.findOne({ email: "testUser@test.com" });
+    expect(created.password).toEqual(testUser.password);
+  });
 
-    const created = users.findOne({email: "axistrotec@outlook.com"})
-    expect(created.password).toEqual("admin21321")
-})
+  it("Should be able to login", () => {
+    const users = db.collection("users");
+
+    const created = users.findOne({ email: "axistrotec@outlook.com" });
+    expect(created.password).toEqual("admin21321");
+  });
+});
